bd3dc02c3ed0fcfb007ebf47d485a9116fd54203
FurkanKhan-Developer <furkhank10@gmail.com>
2021-01-08 14:57:38 +0530

    Add files via upload

---
+++ Example.php
@@ -0,0 +1,36 @@
+<!DOCTYPE>
+<html>
+
+	<head>
+		<title>Untitled</title>
+	</head>
+	
+	
+	
+	<body>
+<h1>My bio<h1>
+<?php
+//My name is Furkan Khan
+/*askdch
+aoufaouf
+aoiufof
+oaidufs*/
+
+echo "My name is Furkan";
+?>	
+<br/>
+<?php echo "who is ali?";  ?>
+<br/>
+<?php echo "whats his age?"  ?>
+<br/>
+<?php echo 5+9+6;  ?>
+	
+	
+	
+	</body>
+
+
+
+
+
+</html>
